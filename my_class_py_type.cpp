b557d6dc08feb0182ad0753e49114c3f7fc08ba3
Stephan Avenwedde <s.avenwedde@gmail.com>
2022-10-31 09:42:34 +0100

    Missing semicolon added

--- my_class_py_type.cpp
+++ my_class_py_type.cpp
@@ -20,7 +20,7 @@ int MyClass_init(PyObject *self, PyObject *args, PyObject *kwds){
     std::cout << "MyClass_init() called!" << std::endl;
     MyClassObject* m = (MyClassObject*)self;
 
-    m->m_value = 123
+    m->m_value = 123;
     m->m_myclass = (MyClass*)PyObject_Malloc(sizeof(MyClass));
 
     if(!m->m_myclass){
